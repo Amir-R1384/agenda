47a900515afd5b68d051a3a6dc01e706d10a9b56
Amir Ranjbar <ranjbaramirreza5786@yahoo.com>
2023-01-09 15:45:14 -0500

    Fixed the bug with date substractor function

--- src/util/getDaysDiff.ts
+++ src/util/getDaysDiff.ts
@@ -1,9 +1,14 @@
 import config from '../config'
 
 export default function getDaysDiff(date: Date | string) {
-	const begining = new Date(config.beginingDay).valueOf()
-	const now = new Date(date).valueOf()
-	const daysDiff = Math.floor((now - begining) / 1000 / 60 / 60 / 24)
+	const begining = new Date(config.beginingDay)
+	const now = new Date(date)
+
+	now.setHours(0)
+	now.setMinutes(0)
+	now.setSeconds(0)
+
+	const daysDiff = Math.floor((now.valueOf() - begining.valueOf()) / 1000 / 60 / 60 / 24)
 
 	return daysDiff
 }
