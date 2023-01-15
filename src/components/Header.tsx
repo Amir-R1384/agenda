9c6362d1ef99a9d51edae4a6eedef89389283c9d
Amir Ranjbar <ranjbaramirreza5786@yahoo.com>
2023-01-15 02:26:00 -0500

    menu icon toggles to X now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Bars3Outline } from '@graywolfai/react-heroicons'
+import { Bars3Outline, XMarkOutline } from '@graywolfai/react-heroicons'
 import { useEffect, useRef, useState } from 'react'
 import DayInfo from './DayInfo'
 import Settings from './Settings'
@@ -19,8 +19,12 @@ export default function Header() {
 			ref={headerRef}
 			className="relative py-2 transition-all duration-700 bg-white border-b dark:border-neutral-700 dark:bg-neutral-800 px-main flex-space-between border-neutral-200">
 			<DayInfo />
-			<button onClick={() => setSettingsOpen(prev => !prev)}>
-				<Bars3Outline className="icon" />
+			<button className="transition-all" onClick={() => setSettingsOpen(prev => !prev)}>
+				{settingsOpen ? (
+					<XMarkOutline className="icon" />
+				) : (
+					<Bars3Outline className="icon" />
+				)}
 			</button>
 
 			<Settings
